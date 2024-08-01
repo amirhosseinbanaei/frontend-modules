c736a8ef212ef04514735c752bda2e8473187205
amirhossein <amirclahroyal09@gmail.com>
2024-08-01 04:05:40 +0330

    React scroll animation

---
+++ Scroll-Animation/scrollAnimationWrapper.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, {
+  useRef,
+  ReactElement,
+  cloneElement,
+} from "react";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+
+interface ScrollAnimationWrapperProps {
+  children: ReactElement;
+  initialClassName: string;
+  animationClassName: string;
+}
+
+const ScrollAnimationWrapper: React.FC<ScrollAnimationWrapperProps> = ({
+  children,
+  animationClassName,
+  initialClassName,
+}) => {
+  const ref = useRef<HTMLElement | null>(null);
+  const isIntersecting = useIntersectionObserver(ref);
+
+  const combinedClassName = `${isIntersecting ? animationClassName : initialClassName}`;
+
+  return cloneElement(children, {
+    ref,
+    className: `${children.props.className ?? ""} ${combinedClassName}`.trim(),
+  });
+};
+
+export default ScrollAnimationWrapper;
+
